e73c627179a2b4aa57f195e82940717993c3c67e
Billcountry <me@billcountry.tech>
2019-07-20 08:35:02 +0300

    Import the css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import { setUp } from "firebase-orient"
+import "materialize-css/dist/css/materialize.min.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 setUp({
