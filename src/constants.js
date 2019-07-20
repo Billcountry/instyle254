9d388d7e2b2b519ee74355f96c336f4e83b51080
Billcountry <me@billcountry.tech>
2019-07-20 13:15:28 +0300

    Some things don't change

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const fonts = { title: "Quando", body: "Judson" }
+
+export const colors = {
+    theme: {
+        red: "#f44336",
+        orange: "#ff9800",
+        teal: "#009688",
+    },
+}
