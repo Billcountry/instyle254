636a351590e0edf8e41ff488bd9db27b53dbd4c5
Billcountry <me@billcountry.tech>
2019-07-20 13:17:40 +0300

    Home route ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import NavBar from "./NavBar"
+import Home from "./home/home"
 
 function App() {
     return (
         <BrowserRouter>
             <NavBar />
+            <Route exact component={Home} path="/" />
         </BrowserRouter>
     )
 }
