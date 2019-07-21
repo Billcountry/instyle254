f1c7637612c81f7d419e4b9f94c5dab2905067e6
Billcountry <me@billcountry.tech>
2019-07-21 10:05:43 +0300

    The Home page ready

--- src/home/home.js
+++ src/home/home.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import background from "../images/home-background.jpg"
 import banner from "../images/home-banner.png"
-import { CardPanel } from "react-materialize"
+import { Link } from "react-router-dom"
 import { colors, fonts } from "../constants"
 
 export default function Home() {
@@ -10,14 +10,15 @@ export default function Home() {
         <div>
             <HomeContainer>
                 <HomeCall>
-                    <p>Show off Kenyan style</p>
+                    <p style={{ marginBottom: 0 }}>Show off Kenyan style</p>
+                    <GalleryButton to="/gallery">Get Started</GalleryButton>
                     <img
                         src={banner}
                         alt=""
                         style={{
                             width: "100%",
                             height: "auto",
-                            marginBottom: 0,
+                            marginBottom: -20,
                         }}
                     />
                 </HomeCall>
@@ -31,25 +32,48 @@ const HomeContainer = styled.div`
     background-size: cover;
     display: flex;
     width: 100%;
-    min-width: calc(100vh - 100px) !important;
+    min-height: calc(100vh - 100px);
+    align-items: flex-end;
     justify-content: center;
+
+    @media screen and (max-width: 800px) {
+        align-items: flex-start;
+    }
     @media screen and (max-width: 600px) {
         margin-top: 5px;
+        min-height: auto;
+        align-items: stretch;
     }
 `
 
 const HomeCall = styled.div`
     width: 800px;
-    margin: 70px 20px 0;
-    padding-bottom: 0%;
+    margin: 70px 0 0;
     background-color: #ffffff;
     font-family: ${fonts.title};
     font-size: 3.2em;
     color: ${colors.theme.teal};
     text-align: center;
+    @media screen and (max-width: 800px) {
+        width: calc(100% - 40px);
+        font-size: 2.5em;
+    }
     @media screen and (max-width: 600px) {
         background-color: #ffffffdd;
         margin: 0;
-        font-size: 2.5em;
+        font-size: 2.2em;
+        width: 100%;
+    }
+`
+
+const GalleryButton = styled(Link)`
+    border-radius: 38px;
+    padding: 10px 20px;
+    color: #ffffff;
+    font-size: 22px;
+    font-family: ${fonts.body};
+    background-color: ${colors.theme.teal};
+    :hover {
+        background-color: ${colors.theme.orange};
     }
 `
