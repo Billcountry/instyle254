baf7eda4aee3007ef9497edd97258d1e6ef7bdb1
Billcountry Mwaniki <me@billcountry.tech>
2019-07-29 15:37:17 +0300

    Added function to request cash

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,16 @@ const functions = require("firebase-functions")
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const lipisha = require("lipisha")
+const secrets = require("./secrets")
+
+const client = new lipisha.Lipisha(secrets.LIPISHA_API_KEY, secrets.LIPISHA_API_SIGNATURE)
+
+
+function request_cash(phone, amount){
+    client.request_settlement(phone, amount, (errror, response)=>{
+        console.log(errror)
+        console.log(response)
+    })
+}
+
