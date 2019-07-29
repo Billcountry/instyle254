8d3fc54e3e7853d167385664592b096bff8a1ff4
Billcountry Mwaniki <me@billcountry.tech>
2019-07-29 20:21:59 +0300

    Galery now with categories

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom"
 import NavBar from "./NavBar"
 import Home from "./home/home"
 import Gallery from "./gallery/gallery"
+import Categories from "./gallery/categories"
 
 function App() {
     return (
         <BrowserRouter>
             <NavBar />
             <Route exact component={Home} path="/" />
-            <Route exact component={Gallery} path="/gallery" />
+            <Route exact component={Categories} path="/gallery" />
         </BrowserRouter>
     )
 }
---
+++ src/gallery/categories.js
@@ -0,0 +1,57 @@
+import React from "react"
+import styled from "styled-components"
+import { BodyContainer } from "../components/containers"
+import pencil from "../images/pencil.jpg"
+import canvas from "../images/canvas.png"
+import skulpture from "../images/skulpture.png"
+import {Link} from "react-router-dom"
+import {colors} from "../constants"
+
+
+export default function Categories(){
+    const menu_items = [
+        {to: "/gallery/canvas", icon: canvas, title: "Canvas"},
+        {to: "/gallery/pencil", icon: pencil, title: "Pencil Drawing"},
+        {to: "/gallery/skulptures", icon: skulpture, title: "Carved Skulptures"},
+    ]
+    return <BodyContainer>
+        <MainContainer>
+            {
+                menu_items.map(item => {
+                    return <StyledLink key={item.to} to={item.to}>
+                        <h2>{item.title}</h2>
+                        <StyledThumb src={item.icon} alt="" />
+                    </StyledLink>
+                })
+            }
+        </MainContainer>
+    </BodyContainer>
+}
+
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+
+`
+
+const StyledLink = styled(Link)`
+        width: calc(50% - 80px);
+        margin: 20px;
+        padding: 20px;
+        border-radius: 16px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #ffffffee;
+        text-decoration: none;
+        color: ${colors.font.default} !important;
+        &:hover{
+            background-color: #ffffff99;
+        }
+`
+
+const StyledThumb = styled.img`
+    height: 200px !important;
+    width: auto;
+`
