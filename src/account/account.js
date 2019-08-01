b624560e654f84acd30aaa018cee62cce90bb0ab
Billcountry Mwaniki <me@billcountry.tech>
2019-08-01 11:04:59 +0300

    Account is still empty

--- src/account/account.js
+++ src/account/account.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react"
 import Auth from "./auth"
+import { Container, BodyContainer } from "../components/containers"
 
 export default class Account extends Component {
     render() {
-        return <Auth />
+        return (
+            <Auth>
+                <BodyContainer>
+                    <Container>
+                        <h5>Velcome</h5>
+                    </Container>
+                </BodyContainer>
+            </Auth>
+        )
     }
 }
