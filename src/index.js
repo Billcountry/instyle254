80082a891acd75731109c2ea97b8eb1d20a967aa
Billcountry Mwaniki <me@billcountry.tech>
2019-08-02 06:21:54 +0300

    Better toastr config

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import { setUp } from "firebase-orient"
 import App from "./App"
+import "toastr/build/toastr.min.css"
 import * as serviceWorker from "./serviceWorker"
-import {GOOGLE_AUTH} from "./secrets"
+import { GOOGLE_AUTH } from "./secrets"
+import toastr from "toastr"
+
+toastr.options.progressBar = true
+toastr.options.positionClass = "toast-bottom-left"
+toastr.options.timeOut = 12000
+
 setUp({
     appID: GOOGLE_AUTH.APP_ID,
     apiKey: GOOGLE_AUTH.API_KEY,
